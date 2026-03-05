ed43e157eeb70e42ccbdc039e7aa6ca8a80389eb
Hilmy Fausta Pratama <hilmyfaustapratama@gmail.com>
2026-03-05 13:41:01 +0700

    program untuk print dan scan terminal

---
+++ pertemuan_2/sebutnama.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    cout << "Siapa namamu?" << endl;
+    char nama[100];
+    cin >> nama;
+
+    cout << "Namamu adalah: " << nama << endl;
+
+    return 0;
+}
